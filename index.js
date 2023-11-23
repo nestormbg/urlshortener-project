8d8104956773528fc05cc84ad697acf383fdc906
Néstor Benitez <nestormbg1011@gmail.com>
2023-11-23 03:14:27 -0300

    mongodb added to dependencies

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient(process.env.MONGO_URI);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 
+
+app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
 });
 
 app.listen(port, function() {
