19b231af2f2434c4571b98407b0c3f51a2703edd
Néstor Benitez <nestormbg1011@gmail.com>
2023-11-23 03:36:14 -0300

    submitting url into the mongo database

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient } = require('mongodb');
+const dns = require('dns');
+const urlParser = require('url');
 // MongoDB Client configuration
 const client = new MongoClient(process.env.MONGO_URI);
 const db = client.db('urlshortener');
@@ -13,7 +15,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 
@@ -23,7 +25,25 @@ app.get('/', function(req, res) {
 
 // Your first API endpoint
 app.post('/api/shorturl', (req, res) => {
-  res.json({reqBody: req.body});
+  const url = req.body.url;
+  const dnsLookUp = dns.lookup(urlParser.parse(url).hostname, async (err, address) => {
+    if (!address) {
+      res.json({ error: 'invalid url' });
+    } else {
+      const urlCount = await urls.countDocuments({});
+      const urlDoc = {
+        url,
+        short_url: urlCount
+      }
+
+      const result = await urls.insertOne(urlDoc);
+
+      res.json({ 
+        original_url: url,  
+        short_url: urlCount
+      });
+    }
+  });
 });
 
 app.listen(port, function() {
