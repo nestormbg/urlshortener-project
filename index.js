ae686f7c5a9f4514c87a7ca23d8de7d55e183c54
Néstor Benitez <nestormbg1011@gmail.com>
2023-11-23 03:25:33 -0300

    mongodb client configuration

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient } = require('mongodb');
-
+// MongoDB Client configuration
 const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db('urlshortener');
+const urls = db.collection('urls');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-
-
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use('/public', express.static(`${process.cwd()}/public`));
-
 
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
 // Your first API endpoint
-app.post('/api/shorturl', function(req, res) {
+app.post('/api/shorturl', (req, res) => {
+  res.json({reqBody: req.body});
 });
 
 app.listen(port, function() {
